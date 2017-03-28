6b7ad171a5fafdefbc4b50d2c73587f0aec8515b
c0x6a <bundit.jay@gmail.com>
2017-03-29 00:19:56 +0700

    Card class add

---
+++ src/MillionaireLadgabang/src/millionaireladgabang/Card.java
@@ -0,0 +1,17 @@
+package millionaireladgabang;
+
+public class Card {
+    
+    private int id;
+    private String name;
+    private String detail;
+    private int deposit;
+    private int withdraw;
+    private int move;
+
+    public Card(int id, String name, String detail){
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
+    }
+}
