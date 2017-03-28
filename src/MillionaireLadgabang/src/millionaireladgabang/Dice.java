b7cef6b28386ad768cda1c9f2274c63fa2e0114b
c0x6a <bundit.jay@gmail.com>
2017-03-29 00:12:38 +0700

    dice class

--- src/MillionaireLadgabang/src/millionaireladgabang/Dice.java
+++ src/MillionaireLadgabang/src/millionaireladgabang/Dice.java
@@ -9,16 +9,53 @@ public class Dice {
     
     private int[] value;
     private Random generator;
+    private int sum;
 
     public Dice(){
         value = new int[amount];
         generator = new Random();
     }
     
-    public int[] getOddEven(){
+    public int getDice(){
+        return sum;
+    }
+    
+    public void generatorDice(){
+        sum = 0;
         for(int i=0; i<amount; ++i){
             value[i] = generator.nextInt() * dat;
+            sum += value[i];
         }
-        return value;
     }
+    
+    public boolean checkEven(){
+        int even = 0;
+        for(int i=0; i<amount; ++i){
+            if( value[i]%2==0 ){
+                even++;
+            }
+        }
+        return even==amount;
+    }
+    
+    public boolean checkOdd(){
+        int odd = 0;
+        for(int i=0; i<amount; ++i){
+            if( value[i]%2==1 ){
+                odd++;
+            }
+        }
+        return odd==amount;
+    }
+    
+    public boolean checkDouble(){
+        int check = 1;
+        int num = value[0];
+        for(int i=1; i<amount; ++i){
+            if( value[i]==num ){
+                check++;
+            }
+        }
+        return check==amount;
+    }    
 }
