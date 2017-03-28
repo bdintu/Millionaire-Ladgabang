a92dbf9c68fe485852bec977d147c0de75a89553
c0x6a <bundit.jay@gmail.com>
2017-03-29 03:05:13 +0700

    update gitigone
    add .java1~

--- src/MillionaireLadgabang/src/millionaireladgabang/LandMark.java
+++ src/MillionaireLadgabang/src/millionaireladgabang/LandMark.java
@@ -2,4 +2,10 @@ package millionaireladgabang;
 
 public class LandMark {
     
+    public static int amount_lanmark = 4;
+    public static int amount_lanmark_member = 2;
+    
+    
+    
+    
 }
